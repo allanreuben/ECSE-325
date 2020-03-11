4cd7ca7bdf43323d18e66e1bbbe56611547d0afa
Noah-C-Z <nczwack@gmail.com>
2020-03-11 00:56:19 -0400

    Finish converting to 2s complement and fix bugs

--- Lab 2/bits.py
+++ Lab 2/bits.py
@@ -21,29 +21,50 @@ def load_numbers(file_name):
 number_list_x = 'lab2-x.txt'
 number_list_y = 'lab2-y.txt'
 
-def find_min_bits():
+def find_min_bits(number_list):
     min_bits = 0
-    for number in load_numbers(number_list_x):
+    min_dec_bits = 0
+    min_int_bits = 0
+    for number in load_numbers(number_list):
         num_dec_bits = 0
         float_number = float(number)
-        num_int_bits = float_number // 2 + 2
+        num_int_bits = int(float_number // 2 + 2)
         while((float_number % 1) != 0):
             num_dec_bits += 1
             float_number *= 2
-        min_bits_temp = num_int_bits + num_dec_bits
-        min_bits = max(min_bits, min_bits_temp)
-    return min_bits
+        min_dec_bits = max(min_dec_bits, num_dec_bits)
+        min_int_bits = max(min_int_bits, num_int_bits)
+    min_bits = min_int_bits + min_dec_bits
+    print("Decimal bits: " + str(min_dec_bits) + "\nInteger bits: " + str(min_int_bits) +  "\nTotal bits: " + str(min_bits))
+    return min_dec_bits
 
-#print(find_min_bits(load_numbers(number_list_x)))
+min_dec_bits_x = find_min_bits(number_list_x)
+min_dec_bits_y = find_min_bits(number_list_y)
 
-def to_binary():
-    file = open("lab2-x-fixed-point.txt", 'w+')
-    for number in load_numbers(number_list_x):
-        float_number = float(number)
-        while((float_number % 1) != 0):
-            float_number *= 2
-        bin_number = '{0:09b}'.format(int(float_number))
+def to_binary(dec_bits, file, number_list):
+    file = open("lab2-" + file + "-fixed-point.txt", 'w+')
+    for number in load_numbers(number_list):
+        float_number = float(number) * (2**dec_bits)
+        bin_number = '{0:010b}'.format(int(float_number))
+        if ("-" in bin_number):
+            bin_number = twos_complement(bin_number)
         file.write(bin_number + " ")
     file.close()
     
-to_binary()
+def twos_complement(number):
+    number = list(re.sub("\-", "0", number))
+    i = len(number) - 1
+    while (number[i] == '0'):
+        i -= 1
+    i -= 1
+    while (i >= 0):
+        if (number[i] == '0'):
+            number[i] = '1'
+        else:
+            number[i] = '0'
+        i -= 1
+    return "".join(number)
+    
+
+to_binary(min_dec_bits_x, "x", number_list_x)
+to_binary(min_dec_bits_y, "y", number_list_y)
