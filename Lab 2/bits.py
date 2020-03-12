c1bc4f8f51602b030e42529c66815e7294c6753c
Noah-C-Z <nczwack@gmail.com>
2020-03-11 20:40:38 -0400

    Implement the batches portion in the script

--- Lab 2/bits.py
+++ Lab 2/bits.py
@@ -18,17 +18,15 @@ def load_numbers(file_name):
     in_file.close()
     return number_list
 
-number_list_x = 'lab2-x.txt'
-number_list_y = 'lab2-y.txt'
+x_list = load_numbers("lab2-x.txt")
+y_list = load_numbers("lab2-y.txt")
 
 def find_sum():
     sum = 0
     max_number = 0
-    x = load_numbers(number_list_x)
-    y = load_numbers(number_list_y)
     i = 0
-    while (i < len(x)):
-        sum += float(x[i]) * float(y[i])
+    while (i < len(x_list)):
+        sum += float(x_list[i]) * float(y_list[i])
         i += 1
         if (abs(sum) > max_number):
             max_number = sum
@@ -40,7 +38,7 @@ def find_min_bits(number_list):
     min_bits = 0
     min_dec_bits = 0
     min_int_bits = 0
-    for number in load_numbers(number_list):
+    for number in number_list:
         num_dec_bits = 0
         float_number = float(number)
         num_int_bits = int(float_number // 2 + 2)
@@ -51,16 +49,28 @@ def find_min_bits(number_list):
         min_int_bits = max(min_int_bits, num_int_bits)
     min_bits = min_int_bits + min_dec_bits
     print("Decimal bits: " + str(min_dec_bits) + "\nInteger bits: " + str(min_int_bits) +  "\nTotal bits: " + str(min_bits))
-    return min_dec_bits
-
-min_dec_bits_x = find_min_bits(number_list_x)
-min_dec_bits_y = find_min_bits(number_list_y)
+    return (min_dec_bits, min_bits)
+    
+print("~~~Full X list~~~")
+min_dec_bits_x = find_min_bits(x_list)
+print("~~~Full Y list~~~")
+min_dec_bits_y = find_min_bits(y_list)
 
 def to_binary(dec_bits, file, number_list):
     file = open("lab2-" + file + "-fixed-point.txt", 'w+')
-    for number in load_numbers(number_list):
+    for number in number_list:
         float_number = float(number) * (2**dec_bits)
         bin_number = '{0:010b}'.format(int(float_number))
+        if ("-" in bin_number):
+            bin_number = twos_complement(bin_number)
+        file.write(bin_number + "\n")
+    file.close()
+
+def to_binary_batch(dec_bits, total_bits, file, batch, number_list):
+    file = open("lab2-" + file + "-fixed-point-batch" + batch + ".txt", 'w+')
+    for number in number_list:
+        float_number = float(number) * (2**dec_bits)
+        bin_number = ('{0:0' + total_bits + 'b}').format(int(float_number))
         if ("-" in bin_number):
             bin_number = twos_complement(bin_number)
         file.write(bin_number + "\n")
@@ -81,5 +91,19 @@ def twos_complement(number):
     return "".join(number)
     
 
-to_binary(min_dec_bits_x, "x", number_list_x)
-to_binary(min_dec_bits_y, "y", number_list_y)
+to_binary(min_dec_bits_x[0], "x", x_list)
+to_binary(min_dec_bits_y[0], "y", y_list)
+
+for i in range(5):
+    x_batch = []
+    y_batch = []
+    for j in range(i * 200, 200 + i * 200):
+        x_batch.append(x_list[j])
+        y_batch.append(y_list[j])
+    print("~~~Batch " + str(i + 1) + " X list~~~")
+    min_bits_x = find_min_bits(x_batch)
+    print("~~~Batch " + str(i + 1) + " Y list~~~")
+    min_bits_y = find_min_bits(y_batch)
+    to_binary_batch(min_bits_x[0], str(min_bits_x[1]), "x", str(i + 1), x_batch)
+    to_binary_batch(min_bits_y[0], str(min_bits_y[1]), "y", str(i + 1), y_batch)
+
