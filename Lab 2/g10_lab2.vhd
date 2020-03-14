968eda4667b9d614ef601129ad48048b6f963db9
Noah-C-Z <nczwack@gmail.com>
2020-03-13 23:27:00 -0400

    Fixed small mistake in VHDL file

--- Lab 2/g10_lab2.vhd
+++ Lab 2/g10_lab2.vhd
@@ -3,8 +3,8 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity g10_lab2 is
-port ( x : in std_logic_vector (9 downto 0); -- first input, you have to first calculate length
- y : in std_logic_vector (9 downto 0); -- second input, replace ?? with your length
+port ( x : in std_logic_vector (9 downto 0); -- first input
+ y : in std_logic_vector (9 downto 0); -- second input
  N : in std_logic_vector (9 downto 0); -- total number of inputs
  clk : in std_logic; -- clock
  rst : in std_logic; -- asynchronous reset (active-high)
@@ -28,7 +28,7 @@ begin
 		elsif (rising_edge(clk)) then
 			tempMac <= tempMac + signed(x) * signed(y); -- Do the Multiply-accumulate operation
 			i <= i + 1;
-			if (i = 1000) then -- If 1000, then set tempReady to 1 and reset i
+			if (i = unsigned(N)) then -- If equal to the number of inputs, then set tempReady to 1 and reset i
 				tempReady <= '1';
 				i <= 0;
 			else
